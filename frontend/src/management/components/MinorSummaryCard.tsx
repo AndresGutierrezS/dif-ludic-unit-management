86bdeb8353b0492d0d0ab4742559a5ca9abf6fa5
Andres Gutierrez <gerardoandresg2001@gmail.com>
2026-01-06 22:29:29 -0600

    Add initial structure to minor summary card component

--- frontend/src/management/components/MinorSummaryCard.tsx
+++ frontend/src/management/components/MinorSummaryCard.tsx
@@ -2,6 +2,17 @@
 
 export const MinorSummaryCard = () => {
   return (
-    <div>MinorSummaryCard</div>
+    <section className="bg-white my-4 p-5">
+        <div className="flex gap-6">
+            <img src="/images/user.png" alt="User Image" className="w-30 h-30 border-4 rounded-full border-gray-200"/>
+            <div>
+                <p>Juan Perez Lopez</p>
+                <p>Curp: Curp34242343  Edad: 15 anios</p>
+                <p>Sexo: Masculino</p>
+                <p>Activo</p>
+                <p>Registro: 2025-08-01</p>
+            </div>
+        </div>
+    </section>
   )
 }
