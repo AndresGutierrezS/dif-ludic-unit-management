f2baa5b023f4925579198fd18c7758f8017d3c59
Andres Gutierrez <gerardoandresg2001@gmail.com>
2026-01-07 18:33:40 -0600

    Add minor summary card styles

--- frontend/src/management/components/MinorSummaryCard.tsx
+++ frontend/src/management/components/MinorSummaryCard.tsx
@@ -2,17 +2,33 @@
 
 export const MinorSummaryCard = () => {
   return (
-    <section className="bg-white my-4 p-5">
-        <div className="flex gap-6">
-            <img src="/images/user.png" alt="User Image" className="w-30 h-30 border-4 rounded-full border-gray-200"/>
-            <div>
-                <p>Juan Perez Lopez</p>
-                <p>Curp: Curp34242343  Edad: 15 anios</p>
-                <p>Sexo: Masculino</p>
-                <p>Activo</p>
-                <p>Registro: 2025-08-01</p>
-            </div>
+    <section className="bg-white my-6 p-6 rounded-lg shadow-sm">
+      <div className="flex flex-col sm:flex-row gap-6 sm:gap-8 items-center sm:items-start">
+        
+        <img
+          src="/images/user.png"
+          alt="Minor Image"
+          className="w-20 h-20 sm:w-24 sm:h-24 border-4 rounded-full border-gray-200"
+        />
+
+        <div className="text-gray-600 flex flex-col gap-1 text-center sm:text-left">
+          <p className="font-bold text-xl sm:text-2xl text-(--color-principal)">
+            Juan Perez Lopez
+          </p>
+
+          <p className="text-sm">
+            Curp: Curp34242343 · Edad: 15 anios · Sexo: Masculino
+          </p>
+
+          <p className="bg-green-100 text-green-700 p-1 rounded-2xl text-xs w-fit mx-auto sm:mx-0">
+            Activo
+          </p>
+
+          <p className="text-sm">
+            Registro: 2025-08-01
+          </p>
         </div>
+      </div>
     </section>
   )
 }
